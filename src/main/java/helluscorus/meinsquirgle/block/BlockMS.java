d8c684209e9c9f94c945cec25a41e3fd2ece20f5
helluscorus <hellcore@gmail.com>
2015-12-10 15:48:26 +0000

    Readme changes README changes, blocks Blocks

---
+++ src/main/java/helluscorus/meinsquirgle/block/BlockMS.java
@@ -0,0 +1,12 @@
+package helluscorus.meinsquirgle.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockMS extends Block {
+
+    public BlockMS(Material material) {
+
+        super(material);
+    }
+}
