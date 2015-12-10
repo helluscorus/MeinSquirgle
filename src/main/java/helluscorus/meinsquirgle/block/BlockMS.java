101930e3d6058310097f50b42495af06f58faa80
helluscorus <hellcore@gmail.com>
2015-12-10 15:56:49 +0000

    Readme changes README changes, blocks Blocks

--- src/main/java/helluscorus/meinsquirgle/block/BlockMS.java
+++ src/main/java/helluscorus/meinsquirgle/block/BlockMS.java
@@ -1,12 +1,40 @@
 package helluscorus.meinsquirgle.block;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import helluscorus.meinsquirgle.reference.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
 
 public class BlockMS extends Block {
 
     public BlockMS(Material material) {
 
         super(material);
+    }
+
+    public BlockMS() {
+
+        this(Material.rock);
+    }
+
+    @Override
+    public String getUnlocalizedName() {
+
+        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnwrappedUnlocalizedName(super.getUnlocalizedName()));
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister) {
+
+        blockIcon = iconRegister.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf(".") + 1));
+    }
+
+    protected String getUnwrappedUnlocalizedName(String unlocalizedName) {
+
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
     }
 }
